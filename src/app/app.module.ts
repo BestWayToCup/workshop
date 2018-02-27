b8d4159c4203e8c994e6dd2f250e236b13bf86cb
BestWayToCup <36342914+BestWayToCup@users.noreply.github.com>
2018-02-27 03:28:42 +0300

    Added dist/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { OfflineComponent } from './offline/offline.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OfflineComponent
   ],
   imports: [
     BrowserModule,
